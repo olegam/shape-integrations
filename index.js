4ec91d080e0d0d56ae2db8d47ba28d72dce2f372
Ole Gammelgaard Poulsen <ole@shape.dk>
2017-05-12 15:20:06 +0200

    Error handling fix

--- index.js
+++ index.js
@@ -53,18 +53,20 @@ const successResponse = function (context, bodyObject, statusCode = 200) {
     "statusCode":statusCode,
     "body": JSON.stringify(bodyObject)
   }
+  console.log('Successful request with response:', bodyObject)
   context.succeed(res)
 }
 
 const failureResponse = function (context, err, statusCode = 500) {
   const body = {
     status: 'error',
-    err: err || {}
+    err: err.message || {}
   }
   const res = {
     "statusCode":statusCode,
     "body": JSON.stringify(body)
   }
+  console.log('Failing request with error:', err)
   context.succeed(res)
 }
 
